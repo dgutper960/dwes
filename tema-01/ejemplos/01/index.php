741f6f40c9de7ea8e50b8c927a2f6b2d666eb52c
dgutper960 <dgutper960@outlook.es>
2023-09-20 14:50:36 +0200

    primer commit

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Ejemplo 1 - PHP
+Hola mundo
+
+*/
+echo '<h1>';
+echo '<hr>';
+echo '<center>';
+echo 'Hola Mundo PHP';
+echo '<BR>';
+echo 'Soy David Gutiérrez';
+echo '</center>';
+echo '<hr>';
+echo '</h1>';
+echo 'DWES - Curso 22/23' ;
+
+
+?>
