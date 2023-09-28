37f6cbf5e21751a9ec63f356dce638cb45f6b3d3
dgutper960 <dgutper960@outlook.es>
2023-09-28 20:48:38 +0200

    01 fin

--- tema-02/actividades/01/index.php
+++ tema-02/actividades/01/index.php
@@ -1,8 +1,12 @@
 <?php
 
-echo 'David';
-print('&nbsp;Gutiérrez Pérez');
-echo '&nbsp;&nbsp;&nbsp;&nbsp;';
-echo '2DAW';
+echo '<h1>Act-2.1 / Ejercicio 1</h1>';
+echo '<br>';
+echo '<p>"La fotógrafa <b>Ire Lenes</b> pone el objetivo sobre</p>';
+echo '<p>diferentes ciudadanos y elementos lituanos que</p>';
+echo '<p>reflejan la vida del país y su relación con Rusia"</p>';
+echo '<p>
+        Puedes verlo en <a href="https://elpais.com/"> el país.com</a>
+      </p>';
 
 ?>
