aa6327b97ef912fa82493cf12ec07b6d332fc0ff
dgutper960 <dgutper960@outlook.es>
2023-09-29 14:16:44 +0200

    is_null

--- tema-02/ejemplos/14/index.php
+++ tema-02/ejemplos/14/index.php
@@ -3,16 +3,31 @@
     ## Función is_null()  
 
     // variable no definida
-    var_dump(is_null($var)); // muesta error pero la ejevución no para
+    var_dump(is_null($var)); // true -> muesta error pero la ejevución no para
 
     echo '<br>';
 
     // variable difinida con valor null saignado
     // -> Si hacemos $var; -> no llega a definir la variable
     $var=null;
-    var_dump(is_null($var));
+    var_dump(is_null($var)); // true
 
     echo '<br>';
+
+    # Casos
+    $var = 0;
+    var_dump(is_null($var)); // false
+
+    $var = "";
+    var_dump(is_null($var)); // false
+
+    $var = [];
+    var_dump(is_null($var)); // false -> Ya tiene asignado un array (no es la nada)
+
+    
+
+
+
 
 
 
