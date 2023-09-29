fdeb4d336cb256559bb61062274fe185cc6e7d6e
dgutper960 <dgutper960@outlook.es>
2023-09-29 14:34:39 +0200

    isset()

--- tema-02/ejemplos/14/index.php
+++ tema-02/ejemplos/14/index.php
@@ -18,13 +18,56 @@
     $var = 0;
     var_dump(is_null($var)); // false
 
+    echo '<br>';
+
     $var = "";
     var_dump(is_null($var)); // false
+
+    echo '<br>';
 
     $var = [];
     var_dump(is_null($var)); // false -> Ya tiene asignado un array (no es la nada)
 
+    echo '<br>';
+
+    ## Función Isset()
+
+    # La variable debe estar declara y su valor no es Null
+    // -> A diferencia de is_null()
+    # Isset no genera un aviso cuando una variable no es declarada
+    $var; // -> En este caso, PHP se comporta como si la variable no estubiera declarada
+
+    # Casos:
+
+    $var; 
+    var_dump(isset($var)); // false
+
+    echo '<br>';
+
+    $var=0; 
+    var_dump(isset($var)); // true
+
+    echo '<br>';
+
+    $var=[]; 
+    var_dump(isset($var)); // true
+
+    echo '<br>';
+
+    $var = ""; 
+    var_dump(isset($var)); // true
+
+    echo '<br>';
+
+    ## is_empty()
+    // Determina si una variable está vacía
+    // Con variables no definidas da true pero genera aviso
+    // Cadenas con un cero da true
+
     
+
+
+
 
 
 
