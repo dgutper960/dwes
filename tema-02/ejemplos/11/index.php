2c5c98aa439d52b60c1f5f910dae35465aa878a1
dgutper960 <dgutper960@outlook.es>
2023-09-29 12:43:43 +0200

    apuntes

---
+++ tema-02/ejemplos/11/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+// TRABAJAMOS CON UNA VARIABLE QUE NO ESTÁ INICIALIZADA
+
+$var1 = 100;
+$var3 = 200 + $var2;
+echo $var3; // muestra 200
+$var3 = 100 * $var2; 
+echo $var3; //muestra 0
+
+// Nos muestra los errores y la lína
+// Igualmente ejecuta el programa
+
+
+
+?>
