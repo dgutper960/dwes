98ad934261c6fb39ae99f489b43c905ad75024f4
dgutper960 <dgutper960@outlook.es>
2023-10-04 13:56:01 +0200

    proyecto 3

--- tema-02/proyectos/03-proyectiles OptimVistas/views/index.php
+++ tema-02/proyectos/03-proyectiles OptimVistas/views/index.php
@@ -1,15 +1,8 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Plantilla Bootstrap 5.3.2</title>
-
-    <!-- Bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <!-- Bootstrap Icons 1.11.1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+      <?php include 'views/plantilla/head.html' ?>
+      <title>Plantilla Bootstrap 5.3.2</title>
   </head>
   <body>
     <!-- Capa Principal -->
@@ -49,14 +42,9 @@
 
     
     <!-- Pie del documento -->
-    <footer class="footer mt-auto py-3 fixed-bottom bg-light">
-        <div class="container">
-            <span class="text-muted">© 2023
-                David Gutiérrez Pérez - DWES - 2º DAW - Curso 23/24</span>
-        </div>
-    </footer>
+    <?php include 'views/plantilla/footer.html' ?>
 
      <!-- Bootstrap Javascript y popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <?php include 'views/plantilla/javascript.html' ?>
   </body>
 </html>
---
+++ tema-02/proyectos/03-proyectiles OptimVistas/views/resultado.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="es">
+  <head>
+      <?php include 'views/plantilla/head.php' ?>
+      <title>Plantilla Bootstrap 5.3.2</title>
+  </head>
+  <body>
+    <!-- Capa Principal -->
+    <div class="container">
+
+        <!-- HEADER = Información para el SEO -->
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-calculator"></i>        
+            <span class="fs-6">Proyecto 2.1 - Calculadora Básica</span>
+        </header>
+
+        <legend>Resultado</legend>
+        <form method="POST">
+
+          <!-- valor-1 por el usuario -->
+          <table class="table">
+            <thead class="table-light">
+              <tr>
+                <th>Valores Iniciales</th>
+                <th></th>
+              </tr>
+              <tr>
+                <td>Velocidad inicial:</td>
+                <td><?=$v0?> m/s</td>
+              </tr>
+              <td>Ángulo Inclinación</td>
+                <td><?=$angulo?> º</td>
+              </tr>
+              <tr>
+                <th>Resultados</th>
+                <th></th>
+              </tr>
+              <td>Angulos Radianes:</td>
+                <td><?=$a0?> º</td>
+              </tr>
+              <tr>
+                <td>Velocidad Inicial X:</td>
+                <td><?=$vx?> m/s</td>
+              </tr>
+              <tr>
+                <td>Velocidad Inicial Y:</td>
+                <td><?=$vy?> m/s</td>
+              </tr>
+              <tr>
+                <td>Alcance Máximo del Proyectil:</td>
+                <td><?=$xmax?> m</td>
+              </tr>
+              <tr>
+                <td>Tiempo de Vuelo del Proyectil:</td>
+                <td><?=$t?> m</td>
+              </tr>
+              <tr>
+                <td>Altura Máxima del Proyectil:</td>
+                <td><?=$ymax?> m</td>
+              </tr>
+
+            </thead>
+            <tbody>
+
+            </tbody>
+          </table>
+
+          <!-- botones de accion -->
+          <div class="btn-group" role="group">
+
+            <a class="btn btn-primary" href="index.php" role="button">Volver</a>
+
+          </div>
+
+    </div>
+
+    
+    <!-- Pie del documento -->
+    <?php include 'views/plantilla/footer.html' ?>
+
+     <!-- Bootstrap Javascript y popper -->
+     <?php include 'views/plantilla/javascript.html' ?>
+  </body>
+</html>
