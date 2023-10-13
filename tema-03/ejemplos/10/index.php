d6fcc4479d1173df036ecc5e934d14067427b422
dgutper960 <dgutper960@outlook.es>
2023-10-13 14:07:15 +0200

    ejemplos

---
+++ tema-03/ejemplos/10/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$numeros = array (0=>1, 1=> 5);
+
+// funcion que muestra los valores de un array
+print_r($numeros);
+
+// print_r -> en navegador click derecho mostrar cod fuente lo muestra formateado
+
+
+?>
---
+++ tema-03/ejemplos/11/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// declaración en PHP
+$numeros = array (0=> 1, 1=> 5);
+
+
+// declaración alternativa (más usada)
+$numeros = [
+    1, 
+    5
+];
+
+echo $numeros[1]; // muestra 5
+
+// funcion que muestra los valores de un array
+print_r($numeros);
+
+// print_r -> en navegador click derecho mostrar cod fuente lo muestra formateado
+
+
+?>
