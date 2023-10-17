4a0b0e015ff62a458768f92b130068c0426bec87
dgutper960 <dgutper960@outlook.es>
2023-10-17 10:30:56 +0200

    apuntes 17/10

---
+++ tema-03/proyectos/02/libs/crud_funciones.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+    función: delete()
+    descripción: elimina un elemento de la tabla
+    parámetros:
+        - tabla
+        - id del elemento de deseo eliminar
+    salida:
+        - tabla actualizada
+*/
+
+function delete($tabla = [], $id){
+    
+}
+
+
+?>
