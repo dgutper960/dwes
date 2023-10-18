0e6aa528c70055db7cb3388f6dd06e020a3103b9
dgutper960 <dgutper960@outlook.es>
2023-10-18 14:52:09 +0200

    clase 18/10

---
+++ tema-03/proyectos/03/views/viewNuevo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Incluir head -->
+    <?php include 'views/layouts/head.html'; ?>
+</head>
+
+<body>
+    <!-- Capa principal -->
+    <div class="container">
+
+        <!-- cabecera documento -->
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-calculator"></i>
+
+            <span class="fs-6">Librería</span>
+        </header>
+
+        <legend>Tabla Libros</legend>
+
+        <!-- Formulario para añadir el nuevo libro -->
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+
+        <!-- Pié del documento -->
+        <?php include 'views/layouts/footer.html' ?>
+
+    </div>
+
+    <!-- javascript bootstrap 532 -->
+    <?php include 'views/layouts/javascript.html' ?>
+</body>
+
+</html>
