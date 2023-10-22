e49f67b8ed0b4a100b50df0a8aa33fce7ed1c0da
dgutper960 <dgutper960@outlook.es>
2023-10-22 19:29:38 +0200

    correccion vista

--- tema-03/proyectos/05/views/viewMostrar.php
+++ tema-03/proyectos/05/views/viewMostrar.php
@@ -2,63 +2,76 @@
 <html lang="es">
 
 <head>
-    <!-- Usaremos una plantilla, que será usada para todos los proyectos -->
-    <?php include 'layouts/head.html' ?>
+    <?php include 'views/layouts/head.html' ?>
     <title>Proyecto 3.1 - Gestión de libros</title>
-
 </head>
 
 <body>
     <!-- Capa principal -->
     <div class="container">
-        <!-- Cabecera -->
+
+        <!-- cabecera documento -->
         <header class="pb-3 mb-4 border-bottom">
             <i class="bi bi-book-half"></i>
-            <span class="fs-4">Proyecto 3.1 - Gestión de libros</span>
+            <span class="fs-6">Proyecto 3.1 - Gestión de libros</span>
         </header>
-        <legend>Detalles del Libro Seleccionado</legend>
 
-        <!-- Menu principal -->
-        <?php include 'partials/menu_print.php'; ?>
+        <legend>Formulario Mostrar Libro</legend>
 
-        <!-- Muestra los datos de la tabla -->
-        <table class="table table-light">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Título</th>
-                    <th>Autor</th>
-                    <th>Género</th>
-                    <th>Precio</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <!-- Mismo formato a cada campo de la tabla -->
-                    <?php foreach ($libro as $campo): ?> <!-- Asociativo -->
-                        <td>
-                            <?= $campo ?>
-                        </td>
-                    <?php endforeach; ?>
-                </tr>
- 
-            </tbody>
-        </table>
+        <!-- Formulario Editar Libro -->
+        <!-- la variable id está en el model editar -->
+        <!-- Se pasa por la url -->
+        <form action="update.php?id=<?=$id?>" method="POST">
+            <!-- id -->
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Id</label>
+                <input type="text" class="form-control" name="id" value="<?=$libro['id']?>" readonly>
+                <!-- <div class="form-text">Introduzca identificador del libro</div> -->
+            </div>
+            <!-- Título -->
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Título</label>
+                <input type="text" class="form-control" name="titulo" value="<?=$libro['titulo']?>" readonly>
+                <!-- <div class="form-text">Introduzca título libro existente</div> -->
+            </div>
+            <!-- Autor -->
+            <div class="mb-3">
+                <label for="autor" class="form-label">Autor</label>
+                <input type="text" class="form-control" name="autor" value="<?=$libro['autor']?>" readonly>
+                <!-- <div class="form-text">Introduzca Autor del libro</div> -->
+            </div>
+            <!-- Género -->
+            <div class="mb-3">
+                <label for="genero" class="form-label">Género</label>
+                <input type="text" class="form-control" name="genero" value="<?=$libro['genero']?>" readonly>
+                <!-- <div class="form-text">Género del libro</div> -->
+            </div>
+            <!-- Precio -->
+            <div class="mb-3">
+                <label for="precio" class="form-label">Precio (€)</label>
+                <input type="number" class="form-control" name="precio" step="0.01" value="<?=$libro['precio']?>" readonly>
+                <!-- <div class="form-text">Introduzca Precio</div> -->
+            </div>
 
-        <!-- Boton de volver -->
-        <a class="btn btn-secondary" href="index.php" role="button">Volver</a>
 
-        <!-- Pie de documento -->
-        <?php
-        include 'layouts/footer.html';
-        ?>
+            <a class="btn btn-secondary" href="index.php" role="button">Volver</a>
+
+        </form>
+
+        <br>
+        <br>
+        <br>
+
+
+
+
+        <!-- Pié del documento -->
+        <?php include 'views/layouts/footer.html' ?>
+
     </div>
 
-
-    <!-- js bootstrap 532-->
-    <?php
-    include 'layouts/javascript.html';
-    ?>
+    <!-- javascript bootstrap 532 -->
+    <?php include 'views/layouts/javascript.html' ?>
 </body>
 
 </html>
--- tema-03/proyectos/05/views/viewNuevo.php
+++ tema-03/proyectos/05/views/viewNuevo.php
@@ -12,7 +12,7 @@
 
         <!-- cabecera documento -->
         <header class="pb-3 mb-4 border-bottom">
-            <i class="bi bi-calculator"></i>
+            <i class="bi bi-book-half"></i>
             <span class="fs-6">Proyecto 3.1 - Gestión de libros</span>
         </header>
 
