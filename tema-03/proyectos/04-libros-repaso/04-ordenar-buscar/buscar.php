4909081dd13ad761c7d079df6547730ceb30978c
dgutper960 <dgutper960@outlook.es>
2023-11-01 18:43:58 +0100

    libros al 100%

---
+++ tema-03/proyectos/04-libros-repaso/04-ordenar-buscar/buscar.php
@@ -0,0 +1,13 @@
+<?php
+
+# Cargamos el crud_funciones
+include 'libs/crud_funciones.php';
+
+# Cargamos el modelo
+include 'models/model.buscar.php';
+
+# Cargamos la vista
+include 'views/view.index.php';
+
+
+?>
