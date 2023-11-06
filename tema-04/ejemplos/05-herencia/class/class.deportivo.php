8c29b96e983255868ca14adf7bd8cf610ebff8ea
dgutper960 <dgutper960@outlook.es>
2023-11-06 14:39:47 +0100

    ejemplo 5

---
+++ tema-04/ejemplos/05-herencia/class/class.deportivo.php
@@ -0,0 +1,77 @@
+<?php
+
+class Deportivo extends Vehiculo
+{
+
+    /**
+     * Ya tenemos de por sí todos los 
+     * atributos y métodos de Vehículo
+     * --> En la importación hay que guardar la gerarquía
+     */
+    // clases propias de Deportivo
+    private $cilindrada;
+    private $km;
+
+    /**
+     * Si queremos estos atributos en el constructor
+     * hay que hacerlo de la sig manera
+     */
+
+    public function __construct(
+        // atributos de la clase padre
+        $nombre = null,
+        $modelo = null,
+        $velocidad = null,
+        $matricula = null,
+        // parametros de la subclase
+        $cilindrada = null,
+        $km = null,
+    ) {
+        // parametros del constructor clase padre
+        parent::__construct($nombre, $modelo, $velocidad, $matricula);
+        // parametros de la subclase
+        $this->cilindrada = $cilindrada;
+        $this->km = $km;
+    } // la instancia de Deportivo solo ejecuta este constructor
+
+
+    
+
+	/**
+	 * Ya tenemos de por sí todos los
+	 * atributos y métodos de Vehículo
+	 * --> En la importación hay que guardar la gerarquía
+	 * @return mixed
+	 */
+	public function getCilindrada() {
+		return $this->cilindrada;
+	}
+	/**
+	 * Ya tenemos de por sí todos los
+	 * atributos y métodos de Vehículo
+	 * --> En la importación hay que guardar la gerarquía
+	 * @param mixed $cilindrada Ya tenemos de por sí todos los
+	 * @return self
+	 */
+	public function setCilindrada($cilindrada): self {
+		$this->cilindrada = $cilindrada;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getKm() {
+		return $this->km;
+	}
+	
+	/**
+	 * @param mixed $km 
+	 * @return self
+	 */
+	public function setKm($km): self {
+		$this->km = $km;
+		return $this;
+	}
+}
+?>
