911865934e416eeb9f27a09c24e2ebf6d650bef1
dgutper960 <dgutper960@outlook.es>
2023-11-14 10:40:23 +0100

    clase 14/11

---
+++ tema-05/ejemplos/06-mostrar-datos-4/index.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Creamos una conexion al localhost 
+ * -> 127.0.0.1:3306
+ * -> ususario root
+ * -> base de datos fp
+ * 
+ * -> Obtenemos un array de objetos de la clase Alumno
+ * 
+ */
+
+ $servidor = 'localhost'; /** en su defecto podemos poner la ip del servidor */
+ $user = 'root';
+ $pass = '';
+ $bbdd = 'fp';
+
+ # Para establecer la conexión usamos un objeto mysqli
+ $conexion = new mysqli($servidor, $user, $pass, $bbdd);
+
+ /**
+  * Controlamos los errores de la conexion
+  */
+
+  if(mysqli_connect_errno()) {
+    echo 'Nº Error'.$conexion->connect_errno; /** es una propiedad de la clase (NO PERENTESIS) */
+    echo '<br>';
+    echo 'Error en la conexión'. $conexion->connect_error;
+    exit();
+  }
+
+  echo 'conexion establecida con exito';
+
+  /**
+   * HACEMOS UNA CONSULTA A LA BBDD
+   */
+
+   # creo una variable con el comando SQL 
+
+  $sql = 'select * from alumnos order by id';
+
+  # guardamos el resultado usando el método query de la clase mysqli
+  $result = $conexion->query($sql); /** esto retorna un objeto de la clase mysqli_result (con sus propiedades) */
+
+  # vemos unas de las propiedades de objeto
+  echo '<br>';
+  echo 'Numero de registros: '.$result->num_rows;
+  echo '<br>';
+  echo 'Numero de registros: '.$result->field_count;
+  echo '<br>';
+
+  # Creamos una variable para almacenar alumnos
+  // retorna un array de tipo asuciativo
+  $alumno = $result->fetch_object(); // retorna solo el primer alumno, 
+  var_dump($alumno);
+  /** 
+   * Para mostrar los campos de todos los alumnos lo metemos en un bucle
+   */
+  while($alumno = $result->fetch_object()) {
+    echo 'id'.$alumno->id;
+    echo '<br>';
+    echo 'Nombre'.$alumno->nombre;
+    echo '<br>';
+    echo 'email'.$alumno->email;
+    echo '<br>';
+    echo 'Curso'.$alumno->poblacion;
+
+  }
+
+
+
+
+
+
+?>
