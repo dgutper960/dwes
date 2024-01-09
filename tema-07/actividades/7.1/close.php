a6a6d77cd22a4fe83ba114dac45faaaa34bb2111
dgutper960 <dgutper960@outlook.es>
2024-01-09 21:27:33 +0100

    Controlamos la posiblilidad de que alguna de las páginas no sea visitada

--- tema-07/actividades/7.1/close.php
+++ tema-07/actividades/7.1/close.php
@@ -16,6 +16,11 @@ $fecha_visita = new DateTime($_SESSION['fecha_hora_visita']);
 // Una vez caalculada la diferencia entre fechas, le damos el formato deseado
 $_SESSION['duracion_sesion'] = date_diff($fecha_fin, $fecha_visita)->format("%y años, %m meses, %d días, %H horas, %i minutos, %s segundos");
 
+// Controlamos la posiblilidad de que alguna de las páginas no sea visitada
+$_SESSION['num_visitas_home'] = isset($_SESSION['num_visitas_home']) ? $_SESSION['num_visitas_home'] : 0;
+$_SESSION['num_visitas_about'] = isset($_SESSION['num_visitas_about']) ? $_SESSION['num_visitas_about'] : 0;
+$_SESSION['num_visitas_services'] = isset($_SESSION['num_visitas_services']) ? $_SESSION['num_visitas_services'] : 0;
+$_SESSION['num_visitas_events'] = isset($_SESSION['num_visitas_events']) ? $_SESSION['num_visitas_events'] : 0;
 
 
 // Almacenamos el total de visitas del sitio web
