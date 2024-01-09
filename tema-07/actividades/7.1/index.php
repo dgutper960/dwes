08bb744a96c692433a9ae6d591f9452bbecd36db
dgutper960 <dgutper960@outlook.es>
2024-01-09 10:37:48 +0100

    empezamos actividad 7.1

---
+++ tema-07/actividades/7.1/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Creamos la sesion
+ */
+session_start();
+if(isset($_SESSION['num_visitias_home'])){
+    $_SESSION['num_visitias_home']++;
+}else{
+    $_SESSION['num_visitias_home'] = 1;
+}
+
+if(isset($_SESSION['fecha_hora_visita'])){
+    $_SESSION['fecha_hora_visita']++;
+}else{
+    $_SESSION['fecha_hora_visita'] = 1;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 7.1</title>
+</head>
+<body>
+
+<ul>
+    <li>
+        <a href="#">Home</a>
+    </li>
+    <li>
+        <a href="#">About</a>
+    </li>
+    <li>
+        <a href="#">Services</a>
+    </li>
+    <li>
+        <a href="#">Events</a>
+    </li>
+</ul>
+
+<ul>
+    <li>Página: Home</li>
+    <li>SID: <?=session_id()?></li>
+    <li></li>
+    <li></li>
+    <li>Visitas Home. <?=$_SESSION['fecha_hora_visita']?></li>
+</ul>
+    
+</body>
+</html>
