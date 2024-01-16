390c3c1ad3710965515b8325534412084a2a0024
dgutper960 <dgutper960@outlook.es>
2024-01-16 08:49:29 +0100

    añade elimminar alumnos-validacion

--- tema-07/proyectos/01-alumnos-validacion/mvc-proyect/config/config.php
+++ tema-07/proyectos/01-alumnos-validacion/mvc-proyect/config/config.php
@@ -3,7 +3,7 @@
 
 # Ruta absoluta
 
-define('URL', 'http://localhost/dwes/tema-07/proyectos/01-validacion/mvc-proyect/');
+define('URL', 'http://localhost/dwes/tema-07/proyectos/01-alumnos-validacion/mvc-proyect/');
 
 # Constante de la Base de Datos
 define('HOST', 'localhost');
