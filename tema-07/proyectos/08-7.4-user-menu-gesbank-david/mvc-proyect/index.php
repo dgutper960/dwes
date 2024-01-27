4d6deb15115eda54c1ec9cc05033b8fca0a8f920
dgutper960 <dgutper960@outlook.es>
2024-01-27 23:47:59 +0100

    7.4

--- tema-07/proyectos/08-7.4-user-menu-gesbank-david/mvc-proyect/index.php
+++ tema-07/proyectos/08-7.4-user-menu-gesbank-david/mvc-proyect/index.php
@@ -6,7 +6,7 @@ require_once 'libs/model.php';
 require_once 'libs/view.php';
 require_once 'class/class.cliente.php';
 require_once 'class/class.cuenta.php';
-require_once 'class/class.movimiento.php';
+require_once 'class/class.user.php';
 require_once "libs/lib.php";
 require_once 'libs/app.php';
 require_once 'config/config.php';
--- tema-07/proyectos/08-7.4-user-menu-gesbank-david/mvc-proyect/template/partials/menuAut.php
+++ tema-07/proyectos/08-7.4-user-menu-gesbank-david/mvc-proyect/template/partials/menuAut.php
@@ -20,8 +20,8 @@
       <div class="d-flex">
         <div class="collapse navbar-collapse" id="exCollapsingNavbar">
               <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                  <li class="nav-item"><a href="<?= URL ?>perfil" class="nav-link active"><?=$_SESSION['name_user']?> |</a></li>
-                  <li class="nav-item"><a href="<?= URL ?>logout" class="nav-link active">Logout</a></li>
+                  <li class="nav-item"><a href="<?= URL ?>perfil" class="nav-link active">-  <?=$_SESSION['name_user']?>  -</a></li>
+                  <li class="nav-item"><a href="<?= URL ?>logout" class="nav-link active">-  Logout  -</a></li>
               </ul>
         </div>
       </div>
