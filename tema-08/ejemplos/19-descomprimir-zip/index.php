195852dfc8ca589a4fb2ca07a97f8db84bfa0d00
dgutper960 <dgutper960@outlook.es>
2024-02-02 14:50:33 +0100

    descargas

--- tema-08/ejemplos/19-descomprimir-zip/index.php
+++ tema-08/ejemplos/19-descomprimir-zip/index.php
@@ -5,24 +5,18 @@
  * -> Buscar la directiva y descomentarla eliminando el ;
  */
 
-/// creamos el objeto del la clase zipArchive
-$zip = new ZipArchive();
+ $archivo_zip = "files.zip";
+ // Creamos una instancia de la clase ZipArchive:
+ $zip = new ZipArchive();
+ // Abrimos el archivo zip:
+ if($zip->open($archivo_zip) === true)
+ {
+ $zip->extractTo('./imagenesAfotos/');
+ $zip->close();
 
-// abrimos con el metodo de la clase, si no existe lo crea, si existe lo machaca
-// el método opne devuelve 0 si no puede abrir
-if($zip->open('files.zip', ZipArchive::CREATE) === true){ // decimos que queremos abrirlo para crearlo
-
-    // usamos glob para abrir (en un array) todo el contenido de una carpeta
-    $files = glob('files/*');
-
-
-    // recorremos el array
-    foreach($files as $file){
-        $zip->addFile($file);
-    }
-
-    $zip->close();
-    echo 'archivo descomprimido correctamente';
-} else{
-    echo 'Algo ha salido mal';
-}
+ echo 'Archivo descomprimido';
+ } else {
+ echo 'Error al descomprimir';
+ }
+ 
+?>
