ee974d9c30c06521de8664535364b1e69623ca0a
dgutper960 <dgutper960@outlook.es>
2024-02-21 07:51:17 +0100

    actividades 4

--- tema-12/actividades/actividad_12.4/routes/web.php
+++ tema-12/actividades/actividad_12.4/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// Cargamos el controlador clientes
+use App\Http\Controllers\ClientesController;
+// Cargamos el controlador producto
+use App\Http\Controllers\ProductoController;
+// Cargamos el controlador acount
+use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// RUTAS CLIENTES
+//  Vinculamos cada Ruta con un método en una sola instrucción
+ Route::controller(ClientesController::class)->group(function(){
+    Route::get('/clientes', [ClientesController::class, 'index']);
+    Route::post('/clientes/create', [ClientesController::class, 'create']);
+    Route::post('/clientes/update/{id}', [ClientesController::class, 'update']);
+    Route::get('/clientes/delete/{id1}/{id2?}', [ClientesController::class, 'delete']);
+    Route::get('/clientes/show/{id}', [ClientesController::class, 'show']);
+ });
+
+ // RUTAS PRODUCTO
+ Route::resource('producto', ProductoController::class);
+
+ // RUTAS DE ACCOUNT
+ Route::resource('accounts', AccountController::class)
+ ->names([
+     'index' => 'inicio_david',
+     'create' => 'crear_david',
+     'show' => 'mostrar_david',
+     'edit' => 'editar_david',
+     'update' => 'actualizar_david',
+     'destroy' => 'eliminar_david',
+ ]);
+
