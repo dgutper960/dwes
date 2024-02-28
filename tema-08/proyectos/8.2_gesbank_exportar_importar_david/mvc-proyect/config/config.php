0608f7bd10e15bb2364d9027f314014a6d3c964b
dgutper960 <dgutper960@outlook.es>
2024-02-28 09:41:44 +0100

    sube proyectos 8.1 / 8.2

---
+++ tema-08/proyectos/8.2_gesbank_exportar_importar_david/mvc-proyect/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/dwes/tema-08/proyectos/8.2_gesbank_exportar_importar_david/mvc-proyect/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
