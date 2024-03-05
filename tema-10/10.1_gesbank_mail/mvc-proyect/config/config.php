5f3df5013ab81c452707994deb6d50d44e831e64
dgutper960 <dgutper960@outlook.es>
2024-03-05 17:27:59 +0100

    proy mail

---
+++ tema-10/10.1_gesbank_mail/mvc-proyect/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/dwes-pract/10.1_gesbank_mail/mvc-proyect/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
