e542b9a9b20dff109a0b6a2cc1ed57dfb2c29cd7
dgutper960 <dgutper960@outlook.es>
2024-03-11 14:57:49 +0100

    w

--- tema-12/proyectos/laravel07/resources/views/student/create.blade.php
+++ tema-12/proyectos/laravel07/resources/views/student/create.blade.php
@@ -3,53 +3,81 @@
 @section('titulo', 'Alumnos')
 @section('subtitulo', 'Añadir nuevo Alumno')
 @section('contenido')
-    @include('partials.alerts') 
+    @include('partials.alerts')
     <div class="card">
         <div class="card-header">
-          Formulario Nuevo Alumno
+            Formulario Nuevo Alumno
         </div>
         <div class="card-body">
-           <!-- Formulario  -->
+            <!-- Formulario  -->
 
-            <form action= {{route('clientes.store')}} method="POST">
+            <form action={{ route('clientes.store') }} method="POST">
                 @csrf
-            
-                <!-- Nombre  -->
+
+                <!-- name  -->
                 <div class="mb-3">
-                    <label for="nombre" class="form-label">Nombre</label>
-                    <input type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
-                    @error('nombre')
+                    <label for="name" class="form-label">name</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                        value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    @error('name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
 
-                <!-- Apellidos  -->
+                <!-- lastname  -->
                 <div class="mb-3">
-                    <label for="apellidos" class="form-label">Apellidos</label>
-                    <input type="text" class="form-control @error('apellidos') is-invalid @enderror" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="nombre" autofocus>
-                    @error('apellidos')
+                    <label for="lastname" class="form-label">lastname</label>
+                    <input type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname"
+                        value="{{ old('lastname') }}" required autocomplete="name" autofocus>
+                    @error('lastname')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
-                <!-- Telefono  -->
+                {{-- birth_date --}}
                 <div class="mb-3">
-                    <label for="telefono" class="form-label">Telefono</label>
-                    <input type="tel" class="form-control @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" required autocomplete="nombre" autofocus>
-                    @error('telefono')
+                    <label for="birth_date" class="form-label">birth_date</label>
+                    <input type="date" class="form-control @error('birth_date') is-invalid @enderror" name="birth_date"
+                        value="{{ old('birth_date') }}" required autocomplete="name" autofocus>
+                    @error('birth_date')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
-                <!-- Ciudad  -->
+
+                <!-- Email  -->
                 <div class="mb-3">
-                    <label for="ciudad" class="form-label">Ciudad</label>
-                    <input type="text" class="form-control @error('ciudad') is-invalid @enderror" name="ciudad" value="{{ old('ciudad') }}" required autocomplete="nombre" autofocus>
-                    @error('ciudad')
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" required autocomplete="name" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <!-- phone  -->
+                <div class="mb-3">
+                    <label for="phone" class="form-label">phone</label>
+                    <input type="tel" class="form-control @error('phone') is-invalid @enderror" name="phone"
+                        value="{{ old('phone') }}" required autocomplete="name" autofocus>
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <!-- city  -->
+                <div class="mb-3">
+                    <label for="city" class="form-label">city</label>
+                    <input type="text" class="form-control @error('city') is-invalid @enderror" name="city"
+                        value="{{ old('city') }}" required autocomplete="name" autofocus>
+                    @error('city')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
@@ -58,38 +86,27 @@
                 <!-- Dni  -->
                 <div class="mb-3">
                     <label for="dni" class="form-label">Dni</label>
-                    <input type="text" class="form-control @error('dni') is-invalid @enderror" name="dni" value="{{ old('dni') }}" required autocomplete="nombre" autofocus>
+                    <input type="text" class="form-control @error('dni') is-invalid @enderror" name="dni"
+                        value="{{ old('dni') }}" required autocomplete="name" autofocus>
                     @error('dni')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
-                <!-- Email  -->
-                <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="nombre" autofocus>
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-                </div>
 
-
-                
-            </div>
+        </div>
         {{-- Fin Formulario --}}
-    
-         
-        
+
+
+
         <div class="card-footer text-muted">
-             <!-- Botones de acción --------------------------------------------------->
-            <a class="btn btn-secondary" href="{{ route ('clientes.index')}}" role="button">Cancelar</a>
+            <!-- Botones de acción --------------------------------------------------->
+            <a class="btn btn-secondary" href="{{ route('clientes.index') }}" role="button">Cancelar</a>
             <button type="reset" class="btn btn-danger">Borrar</button>
             <button type="submit" class="btn btn-primary">Añadir</button>
         </div>
-       
+
         </form>
     </div>
 
