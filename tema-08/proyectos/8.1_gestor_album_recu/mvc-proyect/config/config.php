d9f6d244e364035d3f3fe7cddfc8e1aed369185d
dgutper960 <dgutper960@outlook.es>
2024-04-19 08:40:41 +0200

    conexion config.php

--- tema-08/proyectos/8.1_gestor_album_recu/mvc-proyect/config/config.php
+++ tema-08/proyectos/8.1_gestor_album_recu/mvc-proyect/config/config.php
@@ -7,7 +7,7 @@ define('URL', 'http://localhost/dwes/tema-08/proyectos/8.1_gestor_album_recu/mvc
 
 # Constante de la Base de Datos
 define('HOST', 'localhost');
-define('DB', 'fp');
+define('DB', 'album');
 define('USER', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'utf8mb4');
