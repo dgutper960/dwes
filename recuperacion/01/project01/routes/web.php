52deabf3047b0460720cd232b34dc50dcf5b11cf
dgutper960 <dgutper960@outlook.es>
2024-05-06 13:06:39 +0200

    actividad 12.2 finalizada #2

--- recuperacion/01/project01/routes/web.php
+++ recuperacion/01/project01/routes/web.php
@@ -5,3 +5,59 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/', function () {
+//     return 'HolaMundoLaravel';
+// });
+
+
+// Route::get('/student', function () {
+//     return 'Main de Alumnos';
+// });
+
+
+// Route::get('/student/new', function () {
+//     return 'Crear Nuevo Alumno';
+// });
+
+// // Funcion con parámetros
+
+// Route::get('/student/view/{id}', function ($id) {
+//     return "Ver Detalles Alumno: $id";
+// });
+
+// // Funcion con parámetros opcionales
+
+// Route::get('/student/curso/{id}/{curso?}', function ($id, $curso = null) {
+//     return "Ver Detalles Alumno: $id Curso: $curso";
+// });
+
+Route::get('/test', function () {
+    return "David - DWES - 2DAW - Prueba";
+});
+
+Route::get('/api/user', function () {
+    return "La física es el sistema operativo del Universo. Steven R Garman";
+});
+
+Route::get('/{nombre}/{apellidos}', function ($nombre, $apellidos) {
+    return "Usuario: {$nombre} {$apellidos}";
+});
+
+
+Route::get('/user/view/{id?}', function ($id = null) {
+    if (!is_null($id)) {
+        return "Usuario id: {$id}";
+    } else {
+        return "El usuario no está registrado";
+    }
+});
+
+Route::get('/cliente/cuenta/{cliente}/{stock?}', function ($cliente, $cuenta = null) {
+    if(is_null($cuenta)){
+        return "Seleccione una cuenta de {$cliente}";
+    } else {
+        return "Perfil de {$cliente}.<br>Mostrando detalles de la cuenta {$cuenta}";
+    }
+});
