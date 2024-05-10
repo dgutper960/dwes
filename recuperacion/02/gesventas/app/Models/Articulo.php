3a14b7aa6dc60bc8e7e7c7f7245cd24affb6722b
dgutper960 <dgutper960@outlook.es>
2024-05-10 09:22:07 +0200

    crea modelos articulo y categoria

---
+++ recuperacion/02/gesventas/app/Models/Articulo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'articulos';
+
+    protected $fillable = [
+        'id',
+        'Descripcion',
+        'imagen',
+        'categoria_id',
+        'dni',
+        'email',
+        'stock',
+        'Precio_Coste',
+        'Precio_Venta'
+    ];
+
+    public function categorias()
+    {
+        return $this->hasMany('App\Categoria');
+    }
+}
---
+++ recuperacion/02/gesventas/app/Models/Categoria.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categorias';
+
+    protected $fillable = [
+        'id',
+        'categoria',
+        'descripcion'
+    ];
+
+    public function articulos()
+    {
+        return $this->hasMany('App\Articulo');
+    }
+}
