9a16c0ff8c06d0d0e7014e33d3d6b0e23a429ef7
dgutper960 <dgutper960@outlook.es>
2024-05-14 09:20:42 +0200

    plantilla views

---
+++ recuperacion/laravel_CRUD_Students/resources/views/layouts/layout.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<!-- Directivapara el lenguaje según la region del usuario -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Icons Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <title>@yield('titulo') - Laravel Students 1.0</title>
+</head>
+
+<body>
+    @include('partials.menu');
+    <header>
+        <!-- Titulos y subtitulos -->
+        <div class="container">
+            <h1 class="display-7">@yield('cabecera')</h1>
+            <p class="lead">@yield('subcabecera')</p>
+        </div>
+    </header>
+
+    <main>
+        <!-- Contenido principal -->
+        <div class="container">
+            @yield('main')
+        </div>
+    </main>
+
+    <!-- Pié de página -->
+    <footer class="footer mt-auto py-3 fixed-bottom bg-light">
+        <div class="container">
+            <span class="text-muted">© 2024
+                David Gutiérrez Pérez - DWES - 2º DAW - Curso 23/24</span>
+        </div>
+    </footer>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
