a39747281fb999ac4728ca7ab2a2e2396e305259
dgutper960 <dgutper960@outlook.es>
2024-05-15 13:51:58 +0200

    #12 añade botones de accion

--- recuperacion/laravel_CRUD_Students/resources/views/student/home.blade.php
+++ recuperacion/laravel_CRUD_Students/resources/views/student/home.blade.php
@@ -24,21 +24,36 @@
             <th scope="col">Télefono</th>
             <th scope="col">Email</th>
             <th scope="col">Curso</th>
+            <th scope="col">Acciones</th>
         </tr>
     </thead>
     <tbody>
-        <!-- Recorremos los articulos -->
-        @foreach ($students as $student)
+        <!-- Recorremos los alumnos -->
+        @forelse ($students as $student)
             <tr>
                 <th scope="row">{{ $student['id'] }}</th>
-                <td>{{ $student['name']}}</td>
+                <td>{{ $student->name }}</td>
                 <td>{{ $student['lastname'] }}</td>
                 <td>{{ $student['birt_date'] }}</td>
-                <td>{{ $student['phone'] }}</td>
+                <td>{{ $student->phone }}</td>
                 <td>{{ $student['email'] }}</td>
                 <td>{{ $student->course->course }}</td>
+                <td>
+                    <!-- Botones de Acción -->
+                    <a href="{{ route('students.edit', $student->id)}}" title="Editar" class="btn btn-primary btn-sm"><i class="bi bi-pencil"></i></a>
+                    <a href="{{ route('students.show', $student->id)}}" title="Mostrar" class="btn btn-warning btn-sm"><i class="bi bi-eye-fill"></i></a>
+                    <form style="display:inline;" method="POST" action="{{ route('students.destroy', $student->id)}}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('¿Borrar alumno {{$student->name}} de forma irreversible?')"><i class="bi bi-trash-fill"></i></button>
+                    </form>
+                </td>
             </tr>
-        @endforeach
+        @empty
+            <tr>
+                <td colspan="8">No hay estudiantes registrados.</td>
+            </tr>
+        @endforelse
     </tbody>
 </table>
 
