d7b15e41a15efb8024af0b761d710bda389089c3
dgutper960 <dgutper960@outlook.es>
2024-05-15 09:42:34 +0200

    crear migraciones de cursos y estudiantes

---
+++ recuperacion/laravel_CRUD_Students/database/migrations/2024_05_15_072512_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 35);
+            $table->string('lastname', 45);
+            $table->date('birt_date');
+            $table->char('phone', 13)->nullable();
+            $table->string('email', 40)->unique();
+            $table->unsignedBigInteger('course_id');
+            $table->timestamps();
+
+            // REstricción de clave agena
+            $table->foreign('course_id')
+            ->references('id')->on('courses')
+            ->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
